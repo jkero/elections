f685d2239fc6d63e197ca68267b11e0efe1fff6d
jkero+xeon <jacques.keroack@gmail.com>
2023-12-12 12:18:29 -0500

    avec json election quebec

--- scrutin/conversion_json.py
+++ scrutin/conversion_json.py
@@ -21,7 +21,7 @@ import pandas as pd
 from io import StringIO
 class election():
     def basic(self):
-        df1 = pd.read_json('../../resultats.json', lines=True, orient="columns")
+        df1 = pd.read_json('../resultats.json', lines=True, orient="columns")
         #df1.set_index('statistiques', inplace=True)# statistiques ou circonscriptions
         print(df1['statistiques'][0])
 
